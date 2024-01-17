5e1df288197b449f1e25e21034347262096ddb11
gilang mahardhika <gilangmahardhika@gmail.com>
2024-01-18 02:39:51 +0700

    add log

--- controllers/bookcontroller/bookcontroller.go
+++ controllers/bookcontroller/bookcontroller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gilangmahardhika/golang-web/models"
 	"github.com/gilangmahardhika/golang-web/responses"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 )
 
 func Index(c *fiber.Ctx) error {
@@ -22,18 +23,23 @@ func Create(c *fiber.Ctx) error {
 	var book models.Book
 	var response responses.Error
 	if err := c.BodyParser(&book); err != nil {
+		log.Warn(err)
 		response.Message = err
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"data": response,
 		})
+
 	}
 
 	if err := models.DB.Create(&book).Error; err != nil {
+		log.Warn(err)
 		response.Message = err
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"data": response,
 		})
+
 	}
+	log.Info("create book", book)
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"data": book,
 	})
