0ef622c18a12a060c93d6c004a07bb8f1481ed39
gilang mahardhika <gilangmahardhika@gmail.com>
2024-01-18 01:46:32 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gilangmahardhika/golang-web/controllers/bookcontroller"
+	"github.com/gilangmahardhika/golang-web/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	models.ConnectDatabase()
+	app := fiber.New()
+
+	api := app.Group("/api")
+	book := api.Group("/books")
+
+	book.Get("/", bookcontroller.Index)
+	book.Get("/:id", bookcontroller.Show)
+	book.Post("", bookcontroller.Create)
+	book.Put("/:id", bookcontroller.Update)
+	book.Delete("/:id", bookcontroller.Delete)
+
+	app.Listen(":8080")
+}
